dad0c66594877de30f975e242aa5cf248819c871
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-01-09 22:54:36 +0200

    ciklu uzduotis js

---
+++ JavaScript/2 uzduotis/ciklai.js
@@ -0,0 +1,63 @@
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 15; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i += 3) {
+//   console.log([i]);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 4 === 0) {
+//     console.log(i + ' dalinasi is 4 be liekanos');
+//   }
+// }
+// let skaicius = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   //   console.log(i);
+//   skaicius = skaicius + 1;
+// }
+// console.log(skaicius + ' =skaicius');
+
+// 11 uzduotis, zemesni uz 1000
+// let skaicius = 0;
+
+// for (let i = 1; i < 1000; i++) {
+//   if (i % 3 === 0 || i % 5 === 0) {
+//     skaicius = skaicius + i;
+//   }
+// }
+// console.log('skaicius= ' + skaicius);
+
+//fizzBuzz uzduotis
+// for (let i = 1; i <= 100; i++) {
+//   //   console.log(i);
+//   if (i % 3 === 0) {
+//     console.log('Fizz');
+//   }
+//   if (i % 5 === 0) {
+//     console.log('Buzz');
+//   }
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//   }
+// }
+
+//fibonachi uzduotis
+let pirmasSk = 1;
+let antrasSk = 2;
+let suma = pirmasSk + antrasSk;
+
+let cikluSk = 35;
+
+for (let i = 2; i <= cikluSk - 3; i++) {
+  pirmasSk = antrasSk;
+  antrasSk = suma;
+  suma = pirmasSk + antrasSk;
+}
+
+console.log('suma= ' + suma);
