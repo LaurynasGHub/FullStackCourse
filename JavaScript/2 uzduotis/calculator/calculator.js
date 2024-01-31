56f6214484de4de6c3b5c965f778f72dfc83b4b2
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-01-31 21:57:13 +0200

    added functions to buttons

--- JavaScript/2 uzduotis/calculator/calculator.js
+++ JavaScript/2 uzduotis/calculator/calculator.js
@@ -7,23 +7,45 @@ function addition() {
   return num1 + num2;
 }
 
-function substraction(num1, num2) {
+function substraction() {
+  let num1 = getNumberValue(1);
+  let num2 = getNumberValue(2);
+
+  document.getElementById('result').value = num1 - num2;
+
   return num1 - num2;
 }
 
-function multiplication(num1, num2) {
+function multiplication() {
+  let num1 = getNumberValue(1);
+  let num2 = getNumberValue(2);
+
+  document.getElementById('result').value = num1 * num2;
+
   return num1 * num2;
 }
 
-function division(num1, num2) {
-  return num1 * num2;
+function division() {
+  let num1 = getNumberValue(1);
+  let num2 = getNumberValue(2);
+
+  document.getElementById('result').value = num1 / num2;
+
+  return num1 / num2;
 }
 
-function kelimasLaipsniu(num1, laipsnis) {
+function times() {
+  let num1 = getNumberValue(1);
+  let laipsnis = getNumberValue(2);
+
+  console.log(`times`);
   let grazintinas = num1;
-  for (let i = 1; (i = laipsnis); i++) {
+  for (let i = 1; i < laipsnis; i++) {
     grazintinas = num1 * grazintinas;
+
+    console.log('grazintinas=', grazintinas);
   }
+  document.getElementById('result').value = grazintinas;
 }
 
 function getNumberValue(nrId) {
@@ -37,35 +59,4 @@ function getNumberValue(nrId) {
   return Number(selectedBtn.value);
 }
 
-// //trying to sort medis
-// let medis = [2, 24, 54, 23, 21, 9, 5, 6, 43];
-// let biggestNr = 0;
-// let position = -1;
-
-// for (let i = 0; i < medis.length; i++) {
-//   if (medis[i] > biggestNr) {
-//     biggestNr = medis[i];
-//     position = position + 1;
-//   } else {
-//     position = position - 1;
-//   }
-// }
-
-// // console.log('biggestNr=', biggestNr);
-
-// //callback stuff
-// console.log('===');
-// let itemArr = [2, 4, 5, 6, 7];
-
-// function iterateArr(times) {
-//   for (i = 0; i <= times; i++) {
-//     console.log('itemNr=', itemArr[i]);
-//   }
-// }
-
-// let skaicius = 2;
-
-// iterateArr((skaicius) => {
-//   skaicius * 5;
-//   console.log('ter');
-// });
+//add event listener for when the button is clicked? then change the background
